e0d42b9b3580fcc04c8322663f5e0be8c5ec28d4
hrithikkhatik <hrithikkhatik360@gmail.com>
2025-10-02 20:44:15 +0530

    Add files via upload

---
+++ fullcodepython.py
@@ -0,0 +1,24 @@
+#functional programming
+def add(i,j):
+    return i+j
+res = add(1,2)
+print(res)
+def add(i,j):
+    return i+j
+a=add
+res = a(1,2)
+print(res)
+def add(i,j):
+    return i+j
+def call(i,j):
+    return add(i,j)
+res = call(1,2)
+print(res)
+def add(i,j):
+    return i+j
+def call(i,j):
+    return add(i,j)
+def pas(i,j,fn):
+    return fn(i,j)
+res=pas(1,2,call)
+print(res)
