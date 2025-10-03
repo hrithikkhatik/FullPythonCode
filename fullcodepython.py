f79362890839f9edd5cd3412d53e1a12191c77db
hrithikkhatik <hrithikkhatik360@gmail.com>
2025-10-03 12:59:13 +0530

    Update fullcodepython.py

--- fullcodepython.py
+++ fullcodepython.py
@@ -22,3 +22,23 @@ def pas(i,j,fn):
     return fn(i,j)
 res=pas(1,2,call)
 print(res)
+# lambda function
+def add(a):
+    return a+1
+res =add(1)
+print(res)
+
+fun = lambda a:a+1
+res = fun(2)
+print(res)
+
+def add(a,b):
+    return a+b
+res = add(1,2)
+print(res)
+
+fun = lambda a,b:a+b
+res = fun(2,5)
+print(res)
+
+
