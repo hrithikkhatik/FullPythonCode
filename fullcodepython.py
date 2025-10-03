9cfecded0192bdb2cce035651adcdeb5e9936563
hrithikkhatik <hrithikkhatik360@gmail.com>
2025-10-03 14:57:08 +0530

    Update fullcodepython.py

--- fullcodepython.py
+++ fullcodepython.py
@@ -40,5 +40,37 @@ print(res)
 fun = lambda a,b:a+b
 res = fun(2,5)
 print(res)
+#filter
+def even(a):
+    return a%2==0
+numbers = [1,2,3,4,5,6,7]
+ans = filter(even,numbers)
+print(ans)
+
+def even(a):
+    return a%2==0
+numbers = [1,2,3,4,5,6,7]
+ans = list(filter(even,numbers))
+print(ans)
+
+def even(a):
+    return a%2==0
+numbers = [1,2,3,4,5,6,7]
+ans = set(filter(even,numbers))
+print(ans)
+
+def even(a):
+    return a%2==0
+numbers=[1,2,3,4,5,6,7]
+ans = set(filter(lambda a:a%2==0,numbers))
+print(ans)
+
+ans = set(filter(lambda a:a%2==0,range(11)))
+print(ans)
+
+
+
+
+
 
 
